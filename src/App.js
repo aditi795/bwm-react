20d5b2891d68a017db7cfbbdf4a6d3af028a854a
Aditi Desai <55603078+aditi795@users.noreply.github.com>
2020-05-23 18:47:03 -0400

    React basic

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>HelloWorld 
+      <h2>What a nice Day</h2>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App.js'
+import './index.css';
+
+// const h2Element =  React.createElement('h2', null, 'What a nice day');
+// const element = React.createElement('h1', {className: 'title'}, 'HelloWorld', h2Element);
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App/>, rootElement);
