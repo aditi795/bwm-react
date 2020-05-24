789c1caf1e4c4f0dd19689d022b5806895121adb
Aditi Desai <55603078+aditi795@users.noreply.github.com>
2020-05-24 19:25:13 -0400

    Used useState() and implemented counter functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import CounterApp from './components/CounterApp/CounterApp'
 
 const App = () => {
   return (
-    <div>HelloWorld 
-      <h2>What a nice Day</h2>
-    </div>
+    <CounterApp />
   )
 }
 
---
+++ src/components/CounterApp/CounterApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './CounterApp.css';
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+
+  // const stateArray = useState(100);
+  // const mySuperValue = stateArray[0];
+  // const changeMySuperValue = stateArray[1];
+  
+  // const increment = (step) => {
+  //   setCount(count + step);
+  // }
+
+  const increment = (step) => () => setCount(count + step)
+  
+
+  return (
+    <div>
+      <div className="counter-app">
+        <h1 className="value">{count}</h1>
+        <button onClick={increment(1)}>Increment</button>
+        <button onClick={increment(-10)}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default CounterApp;
