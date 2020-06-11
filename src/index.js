7d827de42eadfad164c3a79d7b5b1db3e69ef43b
Aditi Desai <55603078+aditi795@users.noreply.github.com>
2020-06-11 17:50:09 -0400

    Header styling with scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.js'
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './index.scss';
 
 
 // const h2Element =  React.createElement('h2', null, 'What a nice day');
